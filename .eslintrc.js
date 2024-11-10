d00afe23ff7397c93dfd58cec3c5b79db809233b
John <jchill2@gmail.com>
2024-11-10 05:57:13 -0800

    add linting and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+   // .eslintrc.js
+   module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:playwright/recommended',
+      'prettier'
+    ],
+    parserOptions: {
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    plugins: [
+      'playwright'
+    ],
+    rules: {
+      // Add any custom rules here
+    },
+  };
--- tests/playwright/openmct/createAndSearch.e2e.js
+++ tests/playwright/openmct/createAndSearch.e2e.js
@@ -7,18 +7,18 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('Test Create Button and Search @xpath', async ({ page }) => {
-    //Click Create Button
-    await page.locator('//html/body/div[1]/div/div[2]/div[1]/button/span').click();
-    //Click Create SWG
-    await page.locator('//html/body/div[2]/ul/li[19]').click();
-    //Fill Title
-    await page.locator('//html/body/div[2]/div/div/div/form/div[1]/div[1]/div[1]/input').fill('Demo SWG');
-    //Click Save
-    await page.locator('//html/body/div[2]/div/div/div/form/div[2]/div/button[1]').click();
-    //Click Search Input
-    await page.locator('//html/body/div/div/div[2]/div[2]/div[2]/input').click();
-    //Search for SWG
-    await page.locator('//html/body/div/div/div[2]/div[2]/div[2]/input').fill('SWG');
+  //Click Create Button
+  await page.locator('//html/body/div[1]/div/div[2]/div[1]/button/span').click();
+  //Click Create SWG
+  await page.locator('//html/body/div[2]/ul/li[19]').click();
+  //Fill Title
+  await page.locator('//html/body/div[2]/div/div/div/form/div[1]/div[1]/div[1]/input').fill('Demo SWG');
+  //Click Save
+  await page.locator('//html/body/div[2]/div/div/div/form/div[2]/div/button[1]').click();
+  //Click Search Input
+  await page.locator('//html/body/div/div/div[2]/div[2]/div[2]/input').click();
+  //Search for SWG
+  await page.locator('//html/body/div/div/div[2]/div[2]/div[2]/input').fill('SWG');
 });
 
 test('Test Create Button and Search @css', async ({ page }) => {
@@ -29,7 +29,6 @@ test('Test Create Button and Search @id', async ({ page }) => {
   await page.locator('[id="openmct-app"]/div/div[2]/div[1]/button').click();
 
   await page.locator('//*[@id="form-name"]').fill('Demo SWG');
-
 });
 
 test('Test Create Button and Search @pom', async ({ page }) => {
