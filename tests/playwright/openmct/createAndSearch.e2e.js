44c108054db9aac8230d0ab058c8390e61694f2b
John <jchill2@gmail.com>
2025-04-22 16:43:36 -0700

    small changes

--- tests/playwright/openmct/createAndSearch.e2e.js
+++ tests/playwright/openmct/createAndSearch.e2e.js
@@ -7,29 +7,54 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('Test Create Button and Search @xpath', async ({ page }) => {
+//These are subject to ordering on page.
   //Click Create Button
   await page.locator('//html/body/div[1]/div/div[2]/div[1]/button/span').click();
   //Click Create SWG
   await page.locator('//html/body/div[2]/ul/li[19]').click();
   //Fill Title
-  await page.locator('//html/body/div[2]/div/div/div/form/div[1]/div[1]/div[1]/input').fill('Demo SWG');
+  await page.locator('//html/body/div[2]/div/div[2]/div/div/div/div/form/div[1]/div[1]/div[2]/span/span/input').fill('Demo SWG');
   //Click Save
-  await page.locator('//html/body/div[2]/div/div/div/form/div[2]/div/button[1]').click();
+  await page.locator('//html/body/div[2]/div[2]/div/div/div/div/div[2]/button[1]').click();
   //Click Search Input
-  await page.locator('//html/body/div/div/div[2]/div[2]/div[2]/input').click();
+  await page.locator('//*[@id="openmct-app"]/div/div[2]/div[2]/div[2]/input').click();
   //Search for SWG
-  await page.locator('//html/body/div/div/div[2]/div[2]/div[2]/input').fill('SWG');
+  await page.locator('//*[@id="openmct-app"]/div/div[2]/div[2]/div[2]/input').fill('SWG');
 });
 
 test('Test Create Button and Search @css', async ({ page }) => {
-  await page.locator('[aria-label="Create Button"]').click();
+    //These are subject to ordering on the page, but only change on class refactors.
+    //Click Create Button
+    await page.locator('button.c-create-button.c-button--menu.c-button--major.icon-plus').click();
+    //Click Create SWG. Will fail here due to specificity.
+    await page.locator('li.icon-generator-telemetry').click();
+    //Fill Title
+    await page.locator('span.form-control.shell input#form-name').fill('Demo SWG');
+    //Click Save
+    await page.locator('button.c-button.c-button--major.icon-save').click();
+    //Click Search Input
+    await page.locator('input.c-search-input').click();
+    //Fill Search Input
+    await page.locator('input.c-search-input').fill('SWG');
 });
 
 test('Test Create Button and Search @id', async ({ page }) => {
-  await page.locator('[id="openmct-app"]/div/div[2]/div[1]/button').click();
-
-  await page.locator('//*[@id="form-name"]').fill('Demo SWG');
+    //Click Create Button
+    await page.locator('#create-button-label').click();
+    //Click Create SWG. This will fail because no IDs exist.
+    await page.locator('#create-swg-label').click();
+    //Fill Title
+    await page.locator('#form-name').fill('Demo SWG');
 });
+
+test('Test Create Button and Search @data-testid', async ({ page }) => {
+    //Click Create Button
+    await page.locator('data-test-id=create-button').click();
+    //Click Create SWG. This will work.
+    await page.locator('data-test-id=create-swg-label').click();
+    //Fill Title
+    await page.locator('data-test-id=form-name').fill('Demo SWG');
+  });
 
 test('Test Create Button and Search @pom', async ({ page }) => {
   const createPage = new CreatePage(page);
@@ -43,16 +68,12 @@ test('Test Create Button and Search @pom', async ({ page }) => {
   await headerPage.selectObjectResult('Demo SWG');
 });
 
-test('Test Create Button and Search @data-testid', async ({ page }) => {
-  await page.getByTestId('create-button').click();
-});
-
 test('Test Create Button and Search @a11y', async ({ page }) => {
-  await page.getByLabel('Create', { exact: true }).click();
-  await page.getByLabel('Sine Wave Generator').click();
+  await page.getByRole('button', { name: 'Create', exact: true }).click();
+  await page.getByRole('menuitem', { name: 'Sine Wave Generator' }).click();
   await page.getByLabel('Title', { exact: true }).click();
   await page.getByLabel('Title', { exact: true }).fill('Demo SWG');
-  await page.getByLabel('Save').click();
+  await page.getByRole('button', { name: 'Save' }).click();
   await page.getByRole('searchbox', { name: 'Search Input' }).click();
   await page.getByRole('searchbox', { name: 'Search Input' }).fill('SWG');
   await page.getByLabel('Object Results').getByText('Demo SWG').click();
